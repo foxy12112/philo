09ad591f42cdca6b2bebed351e5baf781f062990
Leon Dick <ldick@1-E-32.42heilbronn.de>
2024-07-24 16:42:09 +0200

    created struct

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 10:13:07 by ldick             #+#    #+#             */
-/*   Updated: 2024/07/10 10:16:14 by ldick            ###   ########.fr       */
+/*   Updated: 2024/07/24 16:31:40 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,19 @@
 # include <stdlib.h>
 # include <sys/time.h>
 # include <unistd.h>
+# include "libs.h"
+
+typedef struct s_philo
+{
+	int	forks;
+	int	philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	time_to_die;
+}			t_philo;
+
+int	philo_five(char *argv[]);
+t_philo	*init(char *argv[]);
 
 #endif
--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -1,35 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/10 10:21:27 by ldick             #+#    #+#             */
-/*   Updated: 2024/07/24 16:32:35 by ldick            ###   ########.fr       */
+/*   Created: 2024/07/24 16:22:50 by ldick             #+#    #+#             */
+/*   Updated: 2024/07/24 16:32:02 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../philosophers.h"
+#include "philosophers.h"
 
-int	philo_five(char *argv[])
+t_philo	*init_five(char *argv[])
 {
-	t_philo *philo;
-	
-	philo = init_five(argv);
-	
-}
+	t_philo	*philo;
 
-int	main(int argc, char *argv[])
-{
-	if (argc == 5)
-	{
-		philo_five(argv);
-	}
-	if (argc == 6)
-	{
-		philo_six
-	}
-	close_all;
-	free_all;
+	philo = ft_calloc(sizeof(t_philo), 1);
+	if (!philo)
+		return (ft_printf("malloc error"));
+	philo->philosophers = ft_atoi(argv[1]);
+	philo->forks = philo->philosophers;
 }
